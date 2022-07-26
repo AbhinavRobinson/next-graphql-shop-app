3c41fc290976c18c61669dc348dfdd35ae6e2547
AbhinavRobinson <abhinavrobinson@gmail.com>
2022-07-26 16:48:02 +0530

    feat: list shop items

--- pages/manage-shop.tsx
+++ pages/manage-shop.tsx
@@ -1,10 +1,37 @@
-import { getAccessToken } from '@auth0/nextjs-auth0'
+import { gql, useQuery } from '@apollo/client'
+import { getAccessToken, useUser } from '@auth0/nextjs-auth0'
 import NewShopForm from '../components/newshopform'
+import ShopList from '../components/shoplist'
+
+const GET_SHOPS_BY_OWNERID = gql`
+  query getShopsByOwnerId($ownerId: String!) {
+    getShopsByOwnerId(ownerId: $ownerId) {
+      data {
+        _id
+        name
+      }
+    }
+  }
+`
 
 export default function ManageShop(props: any) {
+  const { user } = useUser()
+  const { data, loading, error } = useQuery(GET_SHOPS_BY_OWNERID, {
+    variables: {
+      ownerId: user?.sub,
+    },
+  })
+
   return (
     <div>
       <NewShopForm accessToken={props.accessToken} />
+      {data ? (
+        <ShopList shops={data.getShopsByOwnerId.data} />
+      ) : (
+        <p>
+          {loading && 'Loading...'} {JSON.stringify(error)}
+        </p>
+      )}
     </div>
   )
 }
